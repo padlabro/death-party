71de451e0a393a769839a7bb684dbd9a5f9bf5da
unknown <deeman@deeman.ki1301.com>
2019-08-09 21:21:51 +0700

    Changed port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = prosecc.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const sock = require('./sock.js');
